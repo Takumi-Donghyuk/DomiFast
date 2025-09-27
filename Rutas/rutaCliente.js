1e0579f44673dc66f370feb6d4cdef869bb268a9
idontknow725 <161983686+idontknow725@users.noreply.github.com>
2025-09-27 12:07:30 -0500

    Delete Rutas directory

--- Rutas/rutaCliente.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('menu_cliente', { usuario: req.session.usuario });
-});
-
-module.exports = router;
--- Rutas/rutaRepartidor.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('menu_repartidor', { usuario: req.session.usuario });
-});
-
-module.exports = router;
