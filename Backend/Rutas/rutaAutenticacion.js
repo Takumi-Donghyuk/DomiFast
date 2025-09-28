a9a28fe3b21cc1c900cf6ff25ffc110489d39946
idontknow725 <161983686+idontknow725@users.noreply.github.com>
2025-09-28 13:34:44 -0500

    Update rutaAutenticacion.js

--- Backend/Rutas/rutaAutenticacion.js
+++ Backend/Rutas/rutaAutenticacion.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const controladorAutenticacion = require('../Controladores/controladorAutenticacion');
 
-
+//Vista login
 router.get('/', (req, res)=> res.render('login'));
+//login
 router.post('/', controladorAutenticacion.login);
+//registro---------
+router.post('/register', controladorAutenticacion.register);
+//-------------
+//logout
 router.get('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
