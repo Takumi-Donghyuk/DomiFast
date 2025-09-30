c0bb7b512e85028dac68d7c66af691cf9e163850
idontknow725 <161983686+idontknow725@users.noreply.github.com>
2025-09-30 12:03:35 -0500

    Update modeloUsuario.js

--- Backend/Modelos/modeloUsuario.js
+++ Backend/Modelos/modeloUsuario.js
@@ -5,18 +5,18 @@ async function buscarPorCorreo(correo) {
   return resultado.rows[0]; 
 }
 //register--------------
-async function crear(usuario) {
+/*async function crear(usuario) {
   const resultado = await pool.query(
     `INSERT INTO usuarios (nombre, usuario, correo, telefono, contrasena, id_rol)
      VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
     [usuario.nombre, usuario.usuario, usuario.correo, usuario.telefono, usuario.contrasena, usuario.id_rol]
   );
   return resultado.rows[0];
-}
+}*/
 //-----------------
 
 module.exports = {
-  buscarPorCorreo,
-  crear
+  buscarPorCorreo/*,
+  crear*/
   //, crear--------lo agregado
 };
